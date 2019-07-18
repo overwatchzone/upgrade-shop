9b263d281b3fdf6637fa4e5cbe64472eabc49ecc
Matthew Hatcher <matthewh@halfpetal.com>
2019-07-17 22:52:58 -0400

    Adding new line to compile script.

--- compile.sh
+++ compile.sh
@@ -2,4 +2,5 @@
 echo "Compiling to UpgradeShop.ows..."
 cd ./OWScript
 python OWScript.py ../UpgradeShop.owpy --save ../UpgradeShop.ows
+echo "--------------------------------"
 echo "UpgradeShop compiled."
